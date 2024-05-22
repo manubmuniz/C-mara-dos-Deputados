11221deb69d4ba5103b4ee2a088db59a0adb2f9e
manubmuniz <manubmuniz@gmail.com>
2024-05-22 15:08:25 -0300

    Update CDD.py

--- CDD.py
+++ CDD.py
@@ -1,2 +1,11 @@
 import streamlit as st
-st.title('Título provisório')
+st.title('Eco Política')
+with st.sidebar:
+  st.header("Eco Política")
+  st.writen("o seu guia sobre o meio ambiente na política")
+  st.caption(Criado por Manuela Muniz, Clarissa Treptow e Maria Julia Figuereido) 
+
+st.write("Nosso site tem como objetivo propor uma democratização da informação de projetos ou não que defendam e protejam mudanças climáticas. Para entender mais clique na nossa aba 'Saiba Mais') 
+tab_deputados, tab_ementas, tab_saibamais = st.tabs(["Deputados", "Ementas", "Saiba mais"])
+         
+         
