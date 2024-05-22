3baf1ebf12b9d96161f3a53d2f3f8c0b8c464084
manubmuniz <manubmuniz@gmail.com>
2024-05-22 15:10:26 -0300

    Update CDD.py

--- CDD.py
+++ CDD.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title('Eco Política')
 with st.sidebar:
   st.header("Eco Política")
-  st.writen("o seu guia sobre o meio ambiente na política")
+  st.write("o seu guia sobre o meio ambiente na política")
   st.caption(Criado por Manuela Muniz, Clarissa Treptow e Maria Julia Figuereido) 
 
 st.write("Nosso site tem como objetivo propor uma democratização da informação de projetos ou não que defendam e protejam mudanças climáticas. Para entender mais clique na nossa aba 'Saiba Mais') 
