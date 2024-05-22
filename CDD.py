8a2f167f9aa21386f59443c5bfac4d451aa4b582
manubmuniz <manubmuniz@gmail.com>
2024-05-22 15:10:56 -0300

    Update CDD.py

--- CDD.py
+++ CDD.py
@@ -5,7 +5,7 @@ with st.sidebar:
   st.write("o seu guia sobre o meio ambiente na política")
   st.caption(Criado por Manuela Muniz, Clarissa Treptow e Maria Julia Figuereido) 
 
-st.write("Nosso site tem como objetivo propor uma democratização da informação de projetos ou não que defendam e protejam mudanças climáticas. Para entender mais clique na nossa aba 'Saiba Mais') 
+st.write("Nosso site tem como objetivo propor uma democratização da informação de projetos ou não que defendam e protejam mudanças climáticas. Para entender mais clique na nossa aba 'Saiba Mais'") 
 tab_deputados, tab_ementas, tab_saibamais = st.tabs(["Deputados", "Ementas", "Saiba mais"])
          
          
